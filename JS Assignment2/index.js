aecf8cbd86d07428d5530757384a809bdb1a4b5c
M-Saad-Sohail <93836177+M-Saad-Sohail@users.noreply.github.com>
2022-11-05 17:35:54 +0500

    Add files via upload

---
+++ JS Assignment2/index.js
@@ -0,0 +1,51 @@
+// CH#9 PROMPT
+
+// var name = prompt("What is your name?")
+// console.log(name)
+
+// var age = prompt("What is your age?","18")
+// console.log(age)
+
+// CH#10 CH#12 CH#13  IF, ELSE & IF ELSE STATEMENTS AND & OR OPERATOR NESTED IF
+
+// var x = prompt("5+5")
+// if(x == 10){
+    
+//     alert("Correct")
+
+// }else{
+//     alert("Wrong Answer!")
+// }
+
+// var y = prompt("In which class you are?")
+// if( y>=5 &y<=10){
+//     alert("you can join!")
+    
+// }else if(y>=2 &y<5){
+//     alert("You can join with 100Rs fees!")
+//  }
+// else{
+//     alert("You cannot join!")
+// } 
+
+// var z = prompt("In which school are you?")
+// if( z=="the educators" ||z=="taqwa model school"){
+//     alert("you can join!")
+    
+// }else if(z=="happy palace school"||z=="peace international school"){
+//     alert("You can join but with req application!")
+//  }
+// else{
+//     alert("You cannot join!")
+// } 
+
+// CH#11 COMPARISION OPERATOR
+
+// var age = prompt("What is your age?")
+// if(age<=18){
+    
+//     alert("You can Join!")
+
+// }else{
+//     alert("You are overaged!")
+// }
